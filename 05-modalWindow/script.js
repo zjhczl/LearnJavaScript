73ca5d90519b44c7d016e9ae07dd80aece18a53d
zjhczl <zhoujie.hczl@gmail.com>
2022-06-25 02:07:04 +0800

    modal window

---
+++ 05-modalWindow/script.js
@@ -0,0 +1,26 @@
+'use strict';
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnClose = document.querySelector('.close-modal');
+const btnsShow = document.querySelectorAll('.show-modal');
+const closeModal = function() {
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+}
+const openModal = function() {
+    // modal.style.display = 'block';
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+}
+for (let i = 0; i < btnsShow.length; i++) {
+    btnsShow[i].addEventListener('click', openModal);
+}
+
+btnClose.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+//监听键盘
+document.addEventListener('keydown', function(e) {
+    if (e.key == 'Escape' && !modal.classList.contains('hidden')) {
+        closeModal();
+    }
+});
